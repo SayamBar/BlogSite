5d769b3ff7223620cfbbedf1ee28151d99ad1776
SayamBar <barsayam@gmail.com>
2023-03-09 14:14:16 +0530

    Navbar and User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+         def configure_permitted_parameters
+              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:fullname, :email, :password, :phone, :bio, :gender)}
+
+              devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :avatar, :password, :current_password, :phone, :bio, :gender)}
+         end
 end
